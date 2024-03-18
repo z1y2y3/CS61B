06303d43da30e8a078b631ff38f42e06952224cd
Your Name <you@example.com>
2024-03-18 23:17:53 +0800

    proj1GOLD fixed

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -3,40 +3,61 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestArrayDequeGold {
-    StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
-    ArrayDequeSolution<Integer> nice = new ArrayDequeSolution<>();
-
+    /**
+     * 随机调用四个函数（1000次调用）（两种add，两种remove）（两个队列一起做相同的调用函数）
+     * 最重要的就是1000次随机函数调用是怎样的序列，不是看学生的四个函数哪个写错了
+     * 每次调用后比较remove的结果。只有remove才能比较
+     * 用标准队列的标准函数size()来判断队列是否为空，为空只做两种add，不为空做
+     */
     @Test
-    public void testAddFirstRemoveLast() {
-        for (int i = 0; i < 10000; i++) {
-            double numberBetweenZeroAndOne = StdRandom.uniform();
-            if (numberBetweenZeroAndOne < 0.5) {
-                sad.addFirst(i);
-                nice.addFirst(i);
+    public void testStudentArrayDeque() {
+        StudentArrayDeque<Integer> testArray = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> stdArray = new ArrayDequeSolution<>();
+        String log = "";
+        for (int i = 0; i < 1000; i++) {
+            if (stdArray.size() == 0) {
+                int addNumber = StdRandom.uniform(1000);
+                int headOrBack = StdRandom.uniform(2);
+                if (headOrBack == 0) {
+                    log = log + "addFirst(" + addNumber + ")\n";
+                    testArray.addFirst(addNumber);
+                    stdArray.addFirst(addNumber);
+                } else {
+                    log = log + "addLast(" + addNumber + ")\n";
+                    testArray.addLast(addNumber);
+                    stdArray.addLast(addNumber);
+                }
             } else {
-                Integer actual = sad.removeLast();
-                Integer expected = nice.removeLast();
-                assertEquals("expected:" + expected +
-                        " but got:" + actual + "\n", expected, actual);
+                int x = StdRandom.uniform(4);
+                int addNumber = StdRandom.uniform(1000);
+                Integer testremoveNumber = 1;
+                Integer stdremoveNumber = 1;
+                switch (x) {
+                    case 0:
+                        log = log + "addFirst(" + addNumber + ")\n";
+                        testArray.addFirst(addNumber);
+                        stdArray.addFirst(addNumber);
+                        break;
+                    case 1:
+                        log = log + "addLast(" + addNumber + ")\n";
+                        testArray.addLast(addNumber);
+                        stdArray.addLast(addNumber);
+                        break;
+                    case 2:
+                        log = log + "removeFirst()\n";
+                        testremoveNumber = testArray.removeFirst();
+                        stdremoveNumber = stdArray.removeFirst();
+                        break;
+                    case 3:
+                        log = log + "removeLast()\n";
+                        testremoveNumber = testArray.removeLast();
+                        stdremoveNumber = stdArray.removeLast();
+                        break;
+                    default:
+                }
+                assertEquals(log, stdremoveNumber, testremoveNumber);
             }
         }
+
     }
-
-    @Test
-    public void testAddLastRemoveFirst() {
-        for (int i = 0; i < 10000; i++) {
-            double numberBetweenZeroAndOne = StdRandom.uniform();
-            if (numberBetweenZeroAndOne < 0.5) {
-                sad.addLast(i);
-                nice.addLast(i);
-            } else {
-                Integer actual = sad.removeFirst();
-                Integer expected = nice.removeFirst();
-                assertEquals("expected:" + expected +
-                        " but got:" + actual + "\n", expected, actual);
-            }
-        }
-    }
-
-
 }
