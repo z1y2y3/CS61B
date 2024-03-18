cae9f4cde1d0288d2d828c5d191aa8c90469efe9
Your Name <you@example.com>
2024-03-18 21:24:38 +0800

    proj1b problem fixed 1

--- proj1b/OffByOne.java
+++
@@ -1,6 +0,0 @@
-public class OffByOne implements CharacterComparator {
-    public boolean equalChars(char x, char y) {
-        int value = y - x;
-        return value == 1 || value == -1;
-    }
-}
